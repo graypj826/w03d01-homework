9dcaef3ad47820809acadd23ee0210af7e81e341
Patrick Gray <patrickgray@Patricks-MacBook-Pro.local>
2018-06-25 20:41:08 -0600

    added jquery to log tamagotchis stats and track age at 1000 for testing purposes

--- js/app.js
+++ js/app.js
@@ -24,8 +24,32 @@ $('#nameButton').ready(function(){
 		$("h2:contains('all')").text(`all about ${namingTama}`)
 		$("#feedTama").text(`Feed ${namingTama}`)
 		$("button:contains('play with')").text(`play with ${namingTama}`)
+		
+		$("h3:contains('hunger:')").text(`hunger: ${tony.hunger}`);
+		$("h3:contains('sleepiness:')").text(`sleepiness: ${tony.sleepiness}`);
+		$("h3:contains('boredom:')").text(`boredom: ${tony.boredom}`);
+		$("h3:contains('age:')").text(`age: ${tony.age}`);
 
+		getOlder();
 
 	}
 
 })
+
+
+const getOlder = () => {
+  // Start an interval and when it reaches 0, increase the round
+  // number
+  const timer = setInterval(() => {
+
+    tony.age++; //how do I increase an objects age?
+    $("h3:contains('age:')").text(`age: ${tony.age}`);
+
+    if(tony.age >= 10){
+      clearInterval(timer);
+    } 
+
+  }, 1000);
+  // setInterval, how do I stop setInterval
+}
+
