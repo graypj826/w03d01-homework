597b3df78fd6b8b719c94a66e2140d83b1665234
Patrick Gray <patrickgray@Patricks-MacBook-Pro.local>
2018-06-25 19:57:55 -0600

    added name change functionality

--- js/app.js
+++ js/app.js
@@ -11,4 +11,21 @@ const tony = new Tamagotchi (1, 1, 1, 1);
 
 //what we need: timer to increase stats for inactivity, reset to 0 each time stats go up or 
 //timer for age? 
-//
+$('#nameButton').ready(function(){
+	$('button').on("click",() => {
+		const $nameInput = $("input:text").val();
+		
+		
+		personalizeTama($nameInput);
+	})
+
+	const personalizeTama = (namingTama) =>{
+		console.log(namingTama);
+		$("h2:contains('all')").text(`all about ${namingTama}`)
+		$("#feedTama").text(`Feed ${namingTama}`)
+		$("button:contains('play with')").text(`play with ${namingTama}`)
+
+
+	}
+
+})
